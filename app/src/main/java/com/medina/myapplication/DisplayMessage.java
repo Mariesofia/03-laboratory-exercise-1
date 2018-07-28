4764493a3477d0b6765e3f751bcdbcc90c3b8d44
Sofia Medina <medinasofiamarie123@gmail.com>
2018-07-28 17:54:05 +0800

    Laboratory exercise

---
+++ app/src/main/java/com/medina/myapplication/DisplayMessage.java
@@ -0,0 +1,4 @@
+package com.medina.myapplication;
+
+class DisplayMessage {
+}
---
+++ app/src/main/java/com/medina/myapplication/MyActivity.java
@@ -0,0 +1,28 @@
+package com.medina.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE="edu.sti.myfirstapp.MESSAGE";
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message =editText.getText().toString();
+
+        Intent= intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
